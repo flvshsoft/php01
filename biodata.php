766b3de46892ab9b3a11eb233268767125228b84
Yura <yuhaya.lissera@gmail.com>
2025-02-25 17:52:13 +0700

    add: biodata

---
+++ biodata.php
@@ -0,0 +1,26 @@
+<?php
+
+$nama = 'Yura';
+$nim = 11151101693;
+$tanggal_lahir = '1998-10-01';
+$tempat_lahir = 'Pasir Pengaraian';
+$no_hp = '085179869992';
+$alamat = 'Cipta Karya';
+?>
+
+<h1>Biodata</h1>
+<div>
+    <h4>Nama : <?php echo $nama; ?></h4>
+</div>
+<div>
+    <h4>NIM : <?php echo $nim; ?></h4>
+</div>
+<div>
+    <h4>Tempat Tanggal Lahir : <?php echo $tempat_lahir . ', ' . $tanggal_lahir; ?></h4>
+</div>
+<div>
+    <h4>No Hp : <?php echo $no_hp; ?></h4>
+</div>
+<div>
+    <h4>Alamat : <?php echo $alamat; ?></h4>
+</div>
