e4cfc15feb560db1367378fcb2491377ce27ffc1
Yura <yuhaya.lissera@gmail.com>
2025-03-12 17:13:47 +0700

    add: php form

---
+++ form/mahasiswa_form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="mahasiswa_lihat.php" method="get">
+        <div class="row">
+            <label for="">Nama Lengkap</label>
+            <input type="text" name="nama_lengkap">
+            <br>
+        </div>
+        <div class="row">
+            <label for="">NIM</label>
+            <input type="text" name="nim">
+            <br>
+        </div>
+
+        <input type="submit" value="Simpan">
+    </form>
+</body>
+
+</html>
---
+++ form/mahasiswa_lihat.php
@@ -0,0 +1,10 @@
+<?php
+$nama_lengkap = $_GET['nama_lengkap'];
+$nim = $_GET['nim'];
+
+echo "Nama:  $nama_lengkap";
+echo '<br>';
+echo $nim;
+echo '<br>';
+
+?>
